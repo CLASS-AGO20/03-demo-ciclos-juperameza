a46591f7c4a7a8086564c450681caa293706268a
juperameza <juperameza@hotmail.com>
2020-12-03 18:45:02 -0600

    added function contarImparesFor

--- src/main.js
+++ src/main.js
@@ -7,11 +7,29 @@ sumarParesFor(){
     return suma;
 }
 
-
-
+contarImparesFor(inicio,fin){
+    let cantidad=0;
+    for(inicio;inicio<=fin;inicio++){
+        if(inicio%2!=0){
+            cantidad++;
+        }
+    }
+    return cantidad;
+}
+sumarParesWhile(){
+    let suma=0;
+    let x=2;
+    while(x<=20){
+        x+=2;
+        suma+=x;
+    }
+    return suma;
+}
 
 
 }
 
 let app = new App();
 console.log(app.sumarParesFor());
+console.log(app.contarImparesFor(1,10));
+console.log(app.sumarParesWhile());
