67b18d86dff370a8db480b0460b0bea932f10517
juperameza <juperameza@hotmail.com>
2020-12-03 18:33:40 -0600

    implemented function sumarParesFor

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-export default class App {}
+export default class App {
+sumarParesFor(){
+    let suma=0;
+    for(let x=2; x<=20;x+=2){
+        suma+=x;
+    }
+    return suma;
+}
+
+
+
+
+
+}
 
 let app = new App();
+console.log(app.sumarParesFor());
